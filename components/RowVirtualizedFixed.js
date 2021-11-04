b2f4276d6bf225255f8ec1711cb04c70e2d41da1
hieucchu <hieu.chuchi@tiki.vn>
2021-11-05 03:38:43 +0700

    setup test demo

---
+++ components/RowVirtualizedFixed.js
@@ -0,0 +1,58 @@
+import { useCallback, useRef } from 'react';
+
+import { useVirtual } from '../lib/react-virtual';
+
+const RowVirtualizerFixed = () => {
+  const parentRef = useRef();
+
+  const rowVirtualizer = useVirtual({
+    size: 100,
+    parentRef,
+    estimateSize: useCallback(() => 35, []),
+    overscan: 5
+  });
+
+  return (
+    <>
+      <div
+        ref={parentRef}
+        className="List"
+        style={{
+          height: '200px',
+          width: '400px',
+          overflow: 'auto'
+        }}
+      >
+        <div
+          style={{
+            height: `${rowVirtualizer.totalSize}px`,
+            width: '100%',
+            position: 'relative'
+          }}
+        >
+          {rowVirtualizer.virtualItems.map((virtualRow) => {
+            return (
+              <div
+                key={virtualRow.index}
+                className={
+                  virtualRow.index % 2 ? 'ListItemOdd' : 'ListItemEven'
+                }
+                style={{
+                  position: 'absolute',
+                  top: `${virtualRow.start}px`,
+                  left: 0,
+                  width: '100%',
+                  height: `${virtualRow.size}px`
+                }}
+              >
+                Row {virtualRow.index}
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default RowVirtualizerFixed;
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,27 @@
-import '../styles/globals.css'
+import { useEffect } from 'react';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    const fpsMeter = () => {
+      let prevTime = performance.now(),
+        frames = 0;
+      requestAnimationFrame(function loop() {
+        const time = performance.now();
+        frames++;
+        if (time > prevTime + 1000) {
+          let fps = Math.round((frames * 1000) / (time - prevTime));
+          prevTime = time;
+          frames = 0;
+          console.log('FPS:', fps);
+        }
+        requestAnimationFrame(loop);
+      });
+    };
+    fpsMeter();
+  }, []);
+
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
